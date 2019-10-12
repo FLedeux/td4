0be3b2a7c88095889073ce55c08f7cc8e4c5f8d0
ledeux2u <ledeux2u@iutm-inf-f36-09.iutmetz.site.univ-lorraine.fr>
2019-10-12 11:54:17 +0200

    a

--- src/launch/Lanch_revue.java
+++ src/launch/Lanch_revue.java
@@ -1,24 +1,33 @@
 package launch;
 
 import java.net.URL;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Lanch_revue extends Application{
 
+	private Label lbl_display;
+	private  TextField tf_taif;
+	private TextField  tf_titre;
+	private TextField tf_descripton;
+	private ComboBox cdd_perio;
+	
+	
+	
 	@Override 
 	public void start(Stage primaryStage) {
 		try {
-			URL fxmlURL=getClass().getResource("/fenetre.fxml");
+			URL fxmlURL=getClass().getResource("revue.fxml");
 			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
 			Node root = fxmlLoader.load();
 			Scene scene = new Scene((VBox) root, 600, 400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Ma première fenêtre JavaFX");
 			primaryStage.show();
@@ -33,6 +42,16 @@ public class Lanch_revue extends Application{
 
 	}
 
+	
+	public void creation() {
+// si booléen de tarif est false alors pas creer : message d'erreur
+	}
+	
+	public void test_num() {
+		//test char par char et fair egaffe a la virgule/point)
+		
+	}
+	
 
 }
 	
