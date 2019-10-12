d1e89b45e174cc76dea8d3a8b21f116a08b9e89f
ledeux2u <ledeux2u@iutm-inf-f36-09.iutmetz.site.univ-lorraine.fr>
2019-10-12 11:00:30 +0200

    test

--- src/launch/Lanch_revue.java
+++ src/launch/Lanch_revue.java
@@ -1,11 +1,38 @@
 package launch;
 
-public class Lanch_revue extends Application {
+import java.net.URL;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Lanch_revue extends Application{
+
+	@Override 
+	public void start(Stage primaryStage) {
+		try {
+			URL fxmlURL=getClass().getResource("/fenetre.fxml");
+			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+			Node root = fxmlLoader.load();
+			Scene scene = new Scene((VBox) root, 600, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Ma première fenêtre JavaFX");
+			primaryStage.show();
+			} catch (Exception e) {
+				e.printStackTrace();
+				}
+		}
+	
+	
 	public static void main(String[] args) {
-		// TODO Stub de la méthode généré automatiquement
+		launch(args);
 
 	}
+
 
 }
 	
