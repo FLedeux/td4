91cfd736db2e266afc6114524c5ddf5e5bc73630
zoqo <zoqo@DESKTOP-HC7SAS8>
2019-10-17 18:52:33 +0200

    manque test quand on selectionne une perio, redef les tostring de perio et revue, et de faire un choix d'opération sur sql ou listememoire

--- src/launch/Lanch_revue.java
+++ src/launch/Lanch_revue.java
@@ -12,9 +12,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import metier.Periodicite;
@@ -23,10 +25,11 @@ import metier.Revue;
 public class Lanch_revue extends Application implements Initializable {
 
 @FXML	private Label lbl_display;
-@FXML	private  TextField tf_tarif;
 @FXML	private TextField  tf_titre;
 @FXML	private TextField tf_description;
+@FXML	private TextField tf_tarif;
 @FXML	private ComboBox<Periodicite> cbb_perio;
+@FXML 	private Button b_creer;
 	
 	
 	
@@ -54,31 +57,38 @@ public class Lanch_revue extends Application implements Initializable {
 
 	
 	public void creation() {
-		lbl_display.setText("");
-		boolean creation = true;
-		if(tf_titre.getText().trim()=="") {
-			lbl_display.setText("le titre ne dois pas être vide; ");
-			creation = false;
-		}
-		if((tf_tarif.getText().trim()=="")&&(Double.valueOf(tf_tarif.getText().trim())==0)) {
-			lbl_display.setText(lbl_display.getText()+"le prix ne dois pas être vide ou égale a zéro; ");
-			creation=false;
-		}
-		if(cbb_perio.getValue()==null) {
-			lbl_display.setText(lbl_display.getText()+"il faut choisir une periodicité");
-			creation=false;
-		}
 		DAOFactory daos = DAOFactory.getDAOFactory(Persistance.ListeMemoire);
-		if(creation) {
-			Revue r = new Revue(0,tf_titre.getText(),tf_description.getText(),Double.valueOf(tf_tarif.getText()),tf_titre.getText()+".png",cbb_perio.getValue().getId());
-			daos.getRevueDAO().create(r);
-		}
+		Revue r = new Revue(0,tf_titre.getText(),tf_description.getText(),Double.valueOf(tf_tarif.getText()),tf_titre.getText()+".png",cbb_perio.getValue().getId());
+		daos.getRevueDAO().create(r);
+		lbl_display.setText(r.toString());
 	}
 	
-	public void test_num() {
-//on key type check docu oracle et  verif si la touche appuyé est un diggit(isdigit) ou est un .
+	@FXML
+	public void test_num(KeyEvent key) {
 		
+		boolean consumed = false;
+		if(!Character.isDigit(key.getCharacter().charAt(0))&&(key.getCharacter().charAt(0)!='.')) {
+				key.consume();
+				consumed=true;
+			}
+		else if((key.getCharacter().charAt(0)=='.')&&(tf_tarif.getText().contains("."))){
+				key.consume();
+				consumed=true;
+		}
+		if((!tf_titre.getText().isEmpty()) && ((!consumed)||(!tf_tarif.getText().isEmpty())) && (cbb_perio.getValue()!=null)) {
+			b_creer.setDisable(false);
+		}
+		else b_creer.setDisable(true);
+		}
+	
+	
+	public void test_vide(KeyEvent key) {
+		if(Character.isLetterOrDigit(key.getCharacter().charAt(0))&& (!tf_tarif.getText().isEmpty()) && (cbb_perio.getValue()!=null)) {
+			b_creer.setDisable(false);
+		}
+		else b_creer.setDisable(true);
 	}
+			
 
 
 
